876420e817019a62e70e6edac5e1740adcaee919
RaymondKADuncan <raymond.k.a.duncan@gmail.com>
2018-10-18 16:52:50 -0400

    Trimmed away extraneous files. Added second iteration for project 3

--- ERS/ers-webpage/src/app/pipes/reimb-status.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ReimbStatusPipe } from './reimb-status.pipe';
-
-describe('ReimbStatusPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ReimbStatusPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- ERS/ers-webpage/src/app/pipes/reimb-type.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ReimbTypePipe } from './reimb-type.pipe';
-
-describe('ReimbTypePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ReimbTypePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
