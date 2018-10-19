bf78e58fc0af4d93fdfa799897ef34c3e77c123c
RaymondKADuncan <raymond.k.a.duncan@gmail.com>
2018-10-19 14:07:57 -0400

    Re-added project 3 files

---
+++ Project 3 - RideshareApp/Iteration 2/rideshare-matching-service/src/main/java/com/revature/rideshare/matching/controllers/MatchingTestController.java
@@ -0,0 +1,29 @@
+package com.revature.rideshare.matching.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.rideshare.matching.clients.UserClient;
+
+/**
+ * The Class MatchingTestController.
+ */
+@RestController
+public class MatchingTestController {
+	
+	/** The user client. */
+	@Autowired
+	private UserClient userClient;
+
+	/**
+	 * Tests Matching controller connection.
+	 *
+	 * @return the string
+	 */
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test() {
+		return userClient.findByOfficeAndRole(1, "ADMIN").toString();
+	}
+}
